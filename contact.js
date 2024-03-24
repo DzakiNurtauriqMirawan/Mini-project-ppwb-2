7437e1fa16725b02a6b84b75a87d9dfc43944ddd
DzakiNurtauriqMirawan <126880712+DzakiNurtauriqMirawan@users.noreply.github.com>
2024-03-24 20:45:15 +0800

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,4 @@
+// function handleCardClick(cardTitle) {
+//     alert('Anda mengklik card dengan judul: ' + cardTitle);
+//     // Gantilah alert dengan tindakan yang sesuai untuk card yang diklik
+// }
